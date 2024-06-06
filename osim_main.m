bc6d684052aabaac43cb4d9d2a502f1cad5fe1e0
terryvis <72761571+terryvis@users.noreply.github.com>
2024-06-06 15:53:20 -0700

    replaced geometry model with joint dynamics model

--- osim_main.m
+++ osim_main.m
@@ -12,17 +12,17 @@ model = Model('arm26.osim');
 state = model.initSystem();
 
 % setup simulation
-N = 250;
+N = 100;
 tf = 10;
 t = linspace(0,tf,N);
 dt = tf/(N-1);
 
 % define x and u as time-series (vectors)
-% u = phi (joint angle)
+% u = torque
+% initialize torque to zero
 u = zeros(N,1);
-u(1) = model.getJointSet().get(1).getCoordinate().getValue(state);
 
-% x = l_0 (fiber length 0 - triceps long)
+% x = phi (joint angle)
 % also define x_dot vector
 x = zeros(N,1);
 x_dot = zeros(N,1);
@@ -32,91 +32,70 @@ num_controls = 1;
 num_st = 1;
 
 % get initial state x0
-x(1) = model.getMuscles().get(3).getFiberLength(state);
-fprintf('x0 = %f\n', x(1));
+x(1) = model.getJointSet().get(1).getCoordinate().getValue(state);
+fprintf('x0 = %f rad\n', x(1));
 
 % define desired trajectory and deriv - can change
-% x_d = -0.01*sin(pi*t) + 0.15;
-x_d = 0.14*ones(1,N);
+x_d = pi/3 + sin(pi*t);
 x_ddot = [0 (x_d(2:end)-x_d(1:end-1))/dt];
+x_d_accel = [0 (x_ddot(2:end)-x_ddot(1:end-1))/dt];
 
-% define param vector
-ord = 2;
-% a = 0.15*ones(ord+1,1); % [a0;a1;a2]
-a = [0.15 -0.1 0.1];
+% define param vector a = [I B K]' with initial guesses
+a = 0.5*ones(3,1);
 
 % define additional params for control/adaptation
 lambda = 1/(dt*10);
-P = diag([1,0,1,1]);
+k = 1;
+P = eye(3);
 
 % run simulation
 for i = 2:N
     % advance timestep
     state.setTime(t(i));
 
-    % calculate phi (u(t)) based on adaptive control policy
+    % calculate torque (u(t)) based on adaptive control policy
     
-    % poly coefficients
-    a0 = a(1);
-    a1 = a(2);
-    a2 = a(3);
-
-    % define u, u_dot for states (Y) based on opensim joint
-    % position/velocity
-    if i >= 3
-        u_dot = (u(i-1) - u(i-2))/dt;
-    else
-        u_dot = 0;
-    end
-    u_prev = model.getJointSet().get(1).getCoordinate().getValue(state);
+    % get params
+    I = a(1);
+    B = a(2);
+    K = a(3);
 
     % define Y (states)
-    Y = [1 u_dot (lambda*u_prev^2 + 2*u_prev*u_dot) (x_ddot(i-1) + lambda*x_d(i-1))];
-    
-    % define controller params
-    a_c = [-a0/a1; -1/lambda; -a2/(lambda*a1); 1/(lambda*a1)];
-    k = -1/(lambda*a1);
+    Y = [(lambda * (x_ddot(i-1) - x_d(i-1)) + x_d_accel(i-1)) x_dot(i-1) x(i-1)];
 
     % define s (x_dot - x_ddot + lambda*x - lambda-x_d)
     s = x_dot(i-1) - x_ddot(i-1) + lambda*(x(i-1) - x_d(i-1));
 
     % compute input (angle)
-    u(i) = Y*a_c - k*s;
-    % u(i) = pi/2 + pi/2*sin(t(i));
+    u(i) = Y*a - k*s;
 
     % update controller params based on adaptation law
-    % a_c = a_c - dt*P*Y'*s;
+    a = a - dt*P*Y'*s;
 
-    % compute a0, a1, a2
-    a1_new = 1/(lambda*a_c(4));
-    a0_new = -a_c(1)*a1_new;
-    a2_new = -a_c(3)*a1_new;
-    a = [a0_new;a1_new;a2_new];
-
-    % set joint angle according to above policy
-    model.getJointSet().get(1).getCoordinate().setValue(state,u(i));
+    % TODO: set external force/torque according to above policy
+    
 
     % let muscle come to equilibrium
+    model.realizeVelocity(state);
     model.equilibrateMuscles(state);
     
     % compute x and x_dot
-    x(i) = model.getMuscles().get(3).getFiberLength(state);
-    x_dot(i) = model.getMuscles().get(3).getFiberVelocity(state);
+    x(i) = model.getJointSet().get(1).getCoordinate().getValue(state);
+    x_dot(i) = model.getJointSet().get(1).getCoordinate().getSpeedValue(state);
 
-    disp(model.getMuscles().get(3).getActivation(state));
 end
 
 
 % plot results
 subplot(2,1,1),plot(t,u)
 subplot(2,1,1),xlabel('Time [sec]')
-subplot(2,1,1),ylabel('Joint angle [rad]')
+subplot(2,1,1),ylabel('Applied torque [Nm]')
 
 subplot(2,1,2),plot(t,x)
 subplot(2,1,2),hold on
 subplot(2,1,2),plot(t,x_d)
 subplot(2,1,2),xlabel('Time [sec]')
-subplot(2,1,2),ylabel('Normalized fiber length')
+subplot(2,1,2),ylabel('Joint angle [rad]')
 subplot(2,1,2),legend('actual','desired')
 
 fprintf('rmse = %f\n', mean((x - x_d').^2))
